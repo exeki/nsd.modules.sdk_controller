ed2b6d07c6c921fa358d3fefc313198d34015985
ekazantsev <ekazantsev@ocs.ru>
2024-04-15 22:26:48 +0400

    изменена сборка. добавлены новые ручки, ответственный за сбор сразу всей ветки или всех веток

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("groovy")
     id("maven-publish")
+    id("nsd_sdk") version "1.3"
 }
 
 group = "ru.kazantsev.nsd.modules"
-version = "1.0.0"
+version = "1.2"
 
 tasks.javadoc{
     options.encoding = "UTF-8"
@@ -37,11 +38,15 @@ repositories {
     mavenLocal()
 }
 
+sdk {
+    addRepositories()
+    addDevDependencies()
+    addAppDependencies()
+}
+
 dependencies {
     implementation ("org.codehaus.groovy:groovy-all:3.0.19")
     implementation ("javax.servlet:servlet-api:2.5")
-    implementation ("ru.kazantsev.nsd.modules:web_api_components:1.0.0")
-    implementation ("ru.kazantsev.nsd.sdk:upper_level_classes:1.0.0")
-    implementation ("ru.kazantsev.nsd.sdk:global_variables:1.0.0")
+    implementation ("ru.kazantsev.nsd.modules:web_api_components:1.0.1")
 }
 
